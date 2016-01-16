34263a30e503c321ed1477be87bd1f7346d7520d
Oleg Yamnikov <bgeyts667@gmail.com>
2016-01-16 22:20:43 +0500

    Refactor

--- catcher/catcher.go
+++ catcher/catcher.go
@@ -57,30 +57,55 @@ func ParseRepo(username string, reponame string) (*Repo, error) {
 func MdHeaderItem(text string) (header string, lvl int, ok bool) {
 	text = strings.TrimSpace(text)
 
-	// There must always be at least 3 chars:
-	// [# H], [+ H], [*H*]
 	if len(text) < 3 {
 		return "", 0, false
 	}
 
-	fst, snd, lst := text[0], text[1], text[len(text)-1]
-
 	// Header
-	if fst == '#' {
+	if text[0] == '#' {
 		return mdHeader(text)
 	}
 
 	// List item
-	if (fst == '*' || fst == '+' || fst == '-') && snd == ' ' {
+	if isMdListItem(text) {
 		return strings.TrimSpace(text[2:]), 8, true
 	}
 
 	// Bold text
-	if fst == '*' && snd != ' ' && lst == '*' {
+	if isMdBoldLine(text) {
 		return strings.TrimSpace(text[1 : len(text)-1]), 7, true
 	}
 
 	return "", 0, false
+}
+
+func isMdListItem(text string) bool {
+	if len(text) < 3 {
+		return false
+	}
+
+	fst, snd := text[0], text[1]
+	if fst != '*' && fst != '+' && fst != '-' {
+		return false
+	}
+	if snd != ' ' {
+		return false
+	}
+
+	return true
+}
+
+func isMdBoldLine(text string) bool {
+	if len(text) < 3 {
+		return false
+	}
+
+	fst, snd, lst := text[0], text[1], text[len(text)-1]
+	if fst != '*' || snd == ' ' || lst != '*' {
+		return false
+	}
+
+	return true
 }
 
 func mdHeader(text string) (header string, lvl int, ok bool) {
@@ -127,16 +152,16 @@ func ReadRepoList(r io.Reader) (list RepoList, err error) {
 
 	scanner := bufio.NewScanner(r)
 	for scanner.Scan() {
+
 		line := strings.TrimSpace(scanner.Text())
+
+		// There must always be at least 3 chars:
+		// [# H], [+ H], [*H*]
 		if len(line) < 3 {
 			continue
 		}
 
-		user, name, ok := MdRepoItem(line)
-
-		fst, snd := line[0], line[1]
-		if (fst == '*' || fst == '+' || fst == '-') && snd == ' ' && ok {
-
+		if user, name, ok := MdRepoItem(line); ok && isMdListItem(line) {
 			titles := make([]string, 0, len(titlesMap))
 			for _, t := range titlesMap {
 				if t != "" {
@@ -147,8 +172,7 @@ func ReadRepoList(r io.Reader) (list RepoList, err error) {
 			continue
 		}
 
-		header, level, ok := MdHeaderItem(line)
-		if ok {
+		if header, level, ok := MdHeaderItem(line); ok {
 			titlesMap[level-1] = header
 			for i := level; i < len(titlesMap); i++ {
 				titlesMap[i] = ""
@@ -156,7 +180,7 @@ func ReadRepoList(r io.Reader) (list RepoList, err error) {
 			continue
 		}
 
-	}
+	} // for scanner.Scan()
 
 	err = scanner.Err()
 	return
