a24c46a403cae6111e2667c42612f3a20b40c672
Oleg Yamnikov <bgeyts667@gmail.com>
2016-01-17 05:42:18 +0500

    Catcher finished

--- catcher/main.go
+++ catcher/main.go
@@ -1,31 +1,88 @@
 package main
 
 import (
+	"encoding/json"
 	"flag"
 	"fmt"
+	"net/http"
 	"os"
+	"time"
 )
 
 const (
-	AwesomeGoReadmeUrl = "https://raw.githubusercontent.com/avelino/awesome-go/master/REDME.md"
+	AwesomeGoReadmeUrl = "https://raw.githubusercontent.com/avelino/awesome-go/master/README.md"
 )
 
 var (
-	tmplFilename string
-	outFilename  string
+	outFilename string
 )
 
 func init() {
-	flag.StringVar(&tmplFilename, "t", "TEMPLATE.md", "template to use for catalog compilation")
-	flag.StringVar(&outFilename, "o", "README.md", "output file")
+	flag.StringVar(&outFilename, "o", "repos.json", "output file")
 }
 
-func errduring(activity string, err interface{}, v ...interface{}) {
-	t := fmt.Sprintf("Error during %v:\n%v\n", activity, err)
-	fmt.Printf(t, v...)
+func fatal(err interface{}) {
+	fmt.Printf("[ERROR] %v\n", err)
+	os.Exit(1)
+}
+
+func fatalf(format string, v ...interface{}) {
+	t := fmt.Sprintf(format, v...)
+	fmt.Printf("[ERROR] %v\n", t)
 	os.Exit(1)
 }
 
 func main() {
 	flag.Parse()
+
+	fmt.Printf("Performing `GET %v`...\n", AwesomeGoReadmeUrl)
+	resp, err := http.Get(AwesomeGoReadmeUrl)
+	if err != nil {
+		fatal(err)
+	}
+	defer resp.Body.Close()
+	if resp.StatusCode != http.StatusOK {
+		fatalf("status code not successfull (%v)", resp.Status)
+	}
+
+	fmt.Printf("Parsing repo catalog from the response body...\n")
+	catalog, err := ReadRepoCatalog(resp.Body)
+	if err != nil {
+		fatal(err)
+	}
+
+	fmt.Printf("Parsed %v blocks of repo entries\n", len(catalog))
+	if len(catalog) == 0 {
+		fatalf("no entries parsed! Must be an error.")
+	}
+
+	for i, block := range catalog {
+		fmt.Printf("Composing data for block [%v/%v]", i+1, len(catalog))
+		for _, entry := range block.Entries {
+			fmt.Print(".")
+			err := ComposeTemplateData(entry)
+			if err != nil {
+				fmt.Println()
+				fatal(err)
+			}
+			// Rate limit
+			time.Sleep(2 * time.Second)
+		}
+		fmt.Println()
+	}
+
+	fmt.Printf("Creating %v...\n", outFilename)
+	out, err := os.Create(outFilename)
+	if err != nil {
+		fatal(err)
+	}
+	defer out.Close()
+
+	fmt.Printf("Encoding data...\n")
+	err = json.NewEncoder(out).Encode(catalog)
+	if err != nil {
+		fatal(err)
+	}
+
+	fmt.Println("COMPLETE")
 }
